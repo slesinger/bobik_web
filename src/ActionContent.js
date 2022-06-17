d3e4c16171b2be60cc52bfe1a4c6f133a21b6ed5
Honza Slesinger <slesinger@gmail.com>
2022-06-17 20:10:05 +0200

    templated actions

--- src/ActionContent.js
+++ src/ActionContent.js
@@ -36,44 +36,89 @@ class ActionContent extends Component {
     document.querySelector('#img-submit').src = '/dash/images/submit-1.gif'
   }
 
+  renderActionInput(data) {
+    return <div className="accordion-item">
+      <h2 className="accordion-header" id={"heading" + data.id}>
+        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#collapse" + data.id} aria-expanded="false" aria-controls={"collapse" + data.id}>
+          {data.heading}
+        </button>
+      </h2>
+      <div id={"collapse" + data.id} className="accordion-collapse collapse" aria-labelledby={"heading" + data.id} data-bs-parent="#accordionExample">
+        <div className="accordion-body">
+          <input type="text" className="form-control" id={data.id} placeholder={data.placeholder} />
+          <button className="btn btn-primary" onClick={() => {
+            this.callRosService(
+              data.serviceName,
+              'bobik_interfaces/msg/String',
+              { query: data.id },
+            )
+          }}>{data.buttonText}</button>
+        </div>
+      </div>
+    </div>
+  }
+
+  renderActionButtons(data) {
+    let buttons = []
+    if (data.buttons !== undefined) {
+      for (let i = 0; i < data.buttons.length; i++) {
+        let button = data.buttons[i]
+        buttons.push(<button className="btn btn-primary" style={{marginRight:"8px"}} onClick={() => {
+          this.callRosService(
+            button.serviceName,
+            'bobik_interfaces/msg/String',
+            { query: button.value },
+          )
+        }}>{button.text}</button>
+        )
+      }
+    }
+    return <div className="accordion-item">
+      <h2 className="accordion-header" id={"heading" + data.id}>
+        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#collapse" + data.id} aria-expanded="false" aria-controls={"collapse" + data.id}>
+          {data.heading}
+        </button>
+      </h2>
+      <div id={"collapse" + data.id} className="accordion-collapse collapse" aria-labelledby={"heading" + data.id} data-bs-parent="#accordionExample">
+        <div className="accordion-body">
+          <span style={{ paddingRight: 40 }}>{data.label}</span>
+          {buttons}
+        </div>
+      </div>
+    </div>
+  }
 
   // To add swipe action to tabs: https://stackoverflow.com/questions/28252089/add-swipe-support-for-bootstrap-tabs
   render() {
     return <div>
       <div className="accordion" id="accordionExample">
 
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="headingOne">
-            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-              Vypnout Bobika
-            </button>
-          </h2>
-          <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-            <div className="accordion-body">
-              <span style={{ paddingRight: 100 }}>Opravdu vypnout?</span>
-              <button className="btn btn-danger" onClick={() => {
-                this.callRosService(
-                  '/bobik_poweroff',
-                  'bobik_interfaces/msg/String',
-                  { data: 'poweroff' },
-                )
-              }}>Vypnout</button>
-            </div>
-          </div>
-        </div>
+        {this.renderActionInput({
+          id: 'action_say',
+          heading: "Řekni něco",
+          placeholder: "co má Bobík vyslovit",
+          buttonText: "Povídej",
+          serviceName: "/bobik_say"
+        })}
 
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="headingTwo">
-            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-              Nejak dalsi akce
-            </button>
-          </h2>
-          <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-            <div className="accordion-body">
-              <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-            </div>
-          </div>
-        </div>
+        {this.renderActionButtons({
+          id: 'action_poweroff',
+          heading: "Vypnout Bobika",
+          label: "Opravdu vypnout?",
+          buttons: [
+            { text: "Vypnout", value: "poweroff", serviceName: "/bobik_poweroff" }
+          ]
+        })}
+
+        {this.renderActionButtons({
+          id: 'action_enableMotors',
+          heading: "Zapnout/vypnout motory",
+          label: "Motory",
+          buttons: [
+            { text: "Zapnout", value: "poweron", serviceName: "/motor_relay" },
+            { text: "Vypnout", value: "poweroff", serviceName: "/motor_relay" }
+          ]
+        })}
 
       </div>
     </div>
